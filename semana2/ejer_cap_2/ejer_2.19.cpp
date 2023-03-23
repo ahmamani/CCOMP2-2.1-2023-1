31869fc2cdee1a160af604db0141dd544d2369fe
DOCENTE-B302 <DOCENTE-B302@UCSP.AULAB>
2023-03-23 08:25:11 -0500

    corrigiendo ejer 2.19

--- semana2/ejer_cap_2/ejer_2.19.cpp
+++ semana2/ejer_cap_2/ejer_2.19.cpp
@@ -18,7 +18,7 @@ int main() {
         min = num1;
     if(num2 < num1 && num2 < num3)
         min = num2;
-    if(num3 < num1 && num3 < num1)
+    if(num3 < num1 && num3 < num2)
         min = num3;
     cout << "Smallest is " << min << endl;
     if(num1 > num2 && num1 > num3)
